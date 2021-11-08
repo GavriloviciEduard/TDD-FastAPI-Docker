ba8c444afc9506753100ae4584f9772011823b0c
Eduard Gavrilovici <eduard.gavrilovici@porsche-engineering.ro>
2021-11-08 12:56:08 +0200

    Starting structure.

--- project/app/config.py
+++ project/app/config.py
@@ -1,18 +1,19 @@
-import os
 import logging
-from pydantic import BaseSettings, AnyUrl
+import os
 from functools import lru_cache
+
+from pydantic import AnyUrl, BaseSettings
 
 log = logging.getLogger("uvicorn")
 
 
 class Settings(BaseSettings):
-  environment: str = os.getenv("ENVIRONMENT", "dev")
-  testing: bool = os.getenv("TESTING", False)
-  database_url: AnyUrl = os.getenv("DATABASE_URL")
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", False)
+    database_url: AnyUrl = os.getenv("DATABASE_URL")
 
 
 @lru_cache()
 def get_settings() -> BaseSettings:
-  log.info("Loading config settings from the environment...")
-  return Settings()
+    log.info("Loading config settings from the environment...")
+    return Settings()
